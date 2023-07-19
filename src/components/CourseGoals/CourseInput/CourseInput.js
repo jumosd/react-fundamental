ad60d6a39c513303cbaa8abb8f2f9dea4c5bb65b
jumosd <jumosd@icloud.com>
2023-07-19 13:52:30 +0900

    동적인 인라인 스타일링

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,38 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length === 0) {
+      setEnteredValue('')
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+
+    //값이 빈스트링일경우 배경이 빨간색으로 해야함
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    } else {
+      setIsValid(true)
+    }
+
     props.onAddGoal(enteredValue);
   };
+  console.log(isValid)
+
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>목표</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler} style={{ backgroundColor: isValid ? 'white' : 'salmon', borderColor: 'red' }} />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
