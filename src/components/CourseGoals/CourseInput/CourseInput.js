a28a459e564fd1e27a7dc65eb960b066ae9de394
jumosd <jumosd@icloud.com>
2023-07-19 18:15:32 +0900

    Styled Components로 props받아서 조건부 렌더링 하기

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { styled } from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+import { isValidDateValue } from '@testing-library/user-event/dist/utils';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -34,13 +36,36 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
-        <label>목표</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">목표 추가하기</Button>
+      <FormControl >
+        <FormContollabel >목표</FormContollabel>
+        <FormControlInput type="text" onChange={goalInputChangeHandler} isValid={isValid} />
+        <Button type="submit">목표 추가하기</Button>
+      </FormControl>
     </form>
   );
 };
 
 export default CourseInput;
+
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+`
+const FormContollabel = styled.label`
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+`
+
+const FormControlInput = styled.input`
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+
+ ${props => !props.isValid && `
+  background-color: salmon;
+  border-color: red;
+ `}
+`
