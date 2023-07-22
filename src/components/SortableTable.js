20cf80fa86fd8257e36accfc039ad1888b680b64
jumosd <jumosd@icloud.com>
2023-07-22 13:31:02 +0900

    객체 데이터 정렬하기

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -3,12 +3,33 @@ import { GoArrowDown, GoArrowUp } from 'react-icons/go';
 import Table from './Table';
 
 function SortableTable(props) {
+  // asc 오름차순 | desc 내림차순
   const [sortOrder, setSortOrder] = useState(null);
+  // 정렬기준 이름 | 색상 | 점수 | 점수의제 곱 |
   const [sortBy, setSortBy] = useState(null);
+
+  console.log("sortOrder", sortOrder)
+  console.log("sortBy", sortBy)
+
   const { config, data } = props;
 
   const handleClick = (label) => {
     // TODO - 정렬 화살표 클릭 시 정렬을 해주는 함수
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc")
+      setSortBy(label)
+    }
+
+    if (sortOrder === null) {
+      setSortOrder('asc');
+      setSortBy(label);
+    } else if (sortOrder === 'asc') {
+      setSortOrder('desc')
+      setSortBy(label)
+    } else if (sortOrder === 'desc') {
+      setSortOrder('asc')
+      setSortBy(label)
+    }
   };
 
   const updatedConfig = config.map((column) => {
@@ -37,6 +58,16 @@ function SortableTable(props) {
     const { sortValue } = config.find((column) => column.label === sortBy);
     sortedData = [...data].sort((a, b) => {
       // TODO - 정렬된 데이터로 바꿔 끼우는 부분 들어갈 comparator 함수
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+      if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder
+      }
     });
   }
 
