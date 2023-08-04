508474e39de5bcb215696f8a47c608a8f5ed6968
jumosd <jumosd@icloud.com>
2023-08-04 09:03:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Counter from './components/Counter';
 import { Provider } from "react-redux"
-
 import store from './store/index';
-import Auth from './components/Auth';
+
 function App() {
   return (
     <Provider store={store}>
       <Counter />
-      <Auth></Auth>
     </Provider>
   );
 }
