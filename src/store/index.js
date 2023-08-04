1955b022167a30aa9bd3e395df1bdcc1d362602b
jumosd <jumosd@icloud.com>
2023-08-04 13:27:30 +0900

    react-toolkit 사용
    - 기존에있던 reducer는 createSlice()로 교체
    - 기존에있던 store는 configureStore()로 교체

--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,68 @@
 import { createStore } from "redux"
+import { configureStore, createSlice } from "@reduxjs/toolkit"
 
 
-const CounterReducerStore = (state = { count: 0, showCounter: true }, action) => {
-    if (action.type === 'increment') {
-        return {
-            ...state,
-            count: state.count + 1,
-        }
-    }
-    if (action.type === 'decrement') {
-        return {
-            ...state,
-            count: state.count - 1,
-        }
-    }
-
-    if (action.type === 'increase') {
-        return {
-            ...state,
-            count: state.count + action.amount
-        }
-    }
-
-    if (action.type === "togle") {
-        return {
-            ...state,
-            showCounter: !state.showCounter
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter
         }
     }
 
+})
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
 
-    return state
-}
+export const couterActions = counterSlice.actions
 
-const store = createStore(CounterReducerStore)
+
+
+
+
+// const CounterReducerStore = (state = { count: 0, showCounter: true }, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             ...state,
+//             count: state.count + 1,
+//         }
+//     }
+//     if (action.type === 'decrement') {
+//         return {
+//             ...state,
+//             count: state.count - 1,
+//         }
+//     }
+
+//     if (action.type === 'increase') {
+//         return {
+//             ...state,
+//             count: state.count + action.amount
+//         }
+//     }
+
+//     if (action.type === "togle") {
+//         return {
+//             ...state,
+//             showCounter: !state.showCounter
+//         }
+//     }
+
+
+//     return state
+// }
+
+// const store = createStore(CounterReducerStore)
 
 export default store
