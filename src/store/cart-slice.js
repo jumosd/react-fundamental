f2fcf761ec024a678a577411788ecaffd5f86d5b
jumosd <jumosd@icloud.com>
2023-08-06 19:56:11 +0900

    카트에 추가하는 로직
    - 새로 추가될 아이템은 payload로 가져옴
    - 중복 체크를 함 array.find()메소드를 이용해 찾음
    - 토탈 수량을 +1 증가
    - 중복이안되어있을경우 아이템을 push()
    - 중복이 되어있을경우 아이템 수량 +1
       총가격 = 총가격 + 새로운가격

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -7,10 +7,40 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
-    addItemToCart(state, action) { /*TODO- 여기에 코드를 작성해 주세요*/},
-    removeItemFromCart(state, action) { /*TODO- 여기에 코드를 작성해 주세요*/},
-  },
-});
+    addItemToCart(state, action) {
+      //새로운아이템 선택
+      const newItem = action.payload
+      //중복된 아이템을 체크해야함 중복되었다면 갯수만 +1 안되었다면 제품을 등록해야함
+      //existing : 기존의, 사용되는 이라는뜻
+      const existingItem = state.items.find((item) =>
+        item.id === newItem.id
+      )
+      // 토탈수량을 1개씩 증가 시켜줌
+      state.totalQuantity++;
+
+      // 기존에있는 아이템이 없을경우 실행되는로직
+      // items에 id, price, quantity, totalprice, name을 추가함
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+
+        })
+        //기존에 아이템이있을경우
+        // 기존아이템수량을 +1 하고
+        // 기존아이템에 총가격 + 새로운 아이템에 가격을 더함
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) { /*TODO- 여기에 코드를 작성해 주세요*/ },
+
+  }
+})
 
 export const cartActions = cartSlice.actions;
 
